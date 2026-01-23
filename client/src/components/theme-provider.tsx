48a034a80a706411199345c653d40e27e98e9f34
Kanishka Agarwal <kanishkaagarwal201@gmail.com>
2026-01-24 01:56:20 +0530

    Revert "Merge pull request #10 from AdityaRaj0317/feature-landing-page"
    This reverts commit 4a75b708517b1c933a27f5a9c0581979c25bed0f, reversing
    changes made to 43e06f7655c128293f3ad517a108e8bbb7ee0b58.

--- client/src/components/theme-provider.tsx
+++ client/src/components/theme-provider.tsx
@@ -1,9 +1,3 @@
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes"
-
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-    return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 import { createContext, useContext, useEffect, useState } from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 
